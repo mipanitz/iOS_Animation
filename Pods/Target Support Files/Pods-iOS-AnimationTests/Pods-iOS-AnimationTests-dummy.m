adb70bf0053da837bda45ced9cf0e5fe0a32af28
Mike Panitz <mike.panitz@gmail.com>
2019-07-27 14:42:16 -0700

    initial commit

---
+++ Pods/Target Support Files/Pods-iOS-AnimationTests/Pods-iOS-AnimationTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_iOS_AnimationTests : NSObject
+@end
+@implementation PodsDummy_Pods_iOS_AnimationTests
+@end
---
+++ iOS-Animation/ViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ViewController.swift
+//  MultiBoardDemo
+//
+//  Created by Michael Panitz on 7/26/19.
+//  Copyright © 2019 Michael Panitz. All rights reserved.
+//
+
+import UIKit
+import Lottie
+
+class ViewController: UIViewController {
+    
+    let animationView = AnimationView(name: "7980-earth")
+    
+    @IBAction func btnPlay(_ sender: UIButton) {
+        animationView.play()
+    }
+    @IBAction func btnStop(_ sender: UIButton) {
+        animationView.stop()
+    }
+    @IBAction func btnPause(_ sender: UIButton) {
+        animationView.pause()
+    }
+    @IBOutlet weak var btnStop: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        animationView.loopMode = .loop
+        
+        animationView.frame = CGRect(x: 0, y: 0, width: 400, height: 400)
+        animationView.center = self.view.center
+        animationView.contentMode = .scaleAspectFill
+        
+        view.addSubview(animationView)
+        
+        animationView.play()
+    }
+}
+
